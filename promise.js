7296fc5b4eebd19af209449d3983eb4dab76aede
muftialfarisy <alfabjp@gmail.com>
2022-09-17 16:04:44 +0700

    mufti alfarisy

--- promise.js
+++ promise.js
@@ -1,8 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = async (hasil) => {
+    try{
+      const data = await promiseTheaterIXX()
+      const data2 = await promiseTheaterVGC()
+        let i = 0
+    data.forEach(datas => {
+        if(datas.hasil ==  hasil){
+          i++
+        }
+      })
+    data2.forEach(data2s => {
+        if(data2s.hasil ==  hasil){
+          i++
+        }
+      })
+      return i
+    }
+    catch(err){
+      console.log(err)
+    }
+  // })
+};
 module.exports = {
   promiseOutput,
 };
